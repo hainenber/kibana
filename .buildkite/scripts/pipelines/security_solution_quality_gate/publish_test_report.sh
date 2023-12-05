1b67a2d7dd420a9f1c942720baaed1bf07cd16e0
dkirchan <diamantis.kirchantzoglou@elastic.co>
2023-12-05 11:16:41 +0200

    Draft reporting work

---
+++ .buildkite/scripts/pipelines/security_solution_quality_gate/publish_test_report.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# npm install -g junit-report-merger
+
+mkdir target
+buildkite-agent artifact download '*.xml' target/
+
+jrm target/combined.xml "target/*.xml"
+
+ls target
+
+cat target/combined.xml
+
+# cat target/kibana-security-solution/cypress/results/output.json
