b632e01f9f0f6de1206c2b023d866458030b3fe4
dkirchan <diamantis.kirchantzoglou@elastic.co>
2023-12-05 14:14:20 +0200

    added curl request for analytics

--- .buildkite/scripts/pipelines/security_solution_quality_gate/publish_test_report.sh
+++ .buildkite/scripts/pipelines/security_solution_quality_gate/publish_test_report.sh
@@ -11,4 +11,21 @@ ls target
 
 cat target/combined.xml
 
-# cat target/kibana-security-solution/cypress/results/output.json
+cd target
+
+BK_ANALYTICS_API_KEY=$(retry 5 5 vault read -field=serverless-sec-sol-cypress-bk-api-key secret/kibana-issues/dev/security-solution-qg-enc-key)
+
+curl \
+  -X POST \
+  -H "Authorization: Token token=\"$BK_ANALYTICS_API_KEY\"" \
+  -F "data=@combined.xml" \
+  -F "format=junit" \
+  -F "run_env[CI]=buildkite" \
+  -F "run_env[key]=$BUILDKITE_BUILD_ID" \
+  -F "run_env[url]=$BUILDKITE_BUILD_URL" \
+  -F "run_env[branch]=$BUILDKITE_BRANCH" \
+  -F "run_env[commit_sha]=$BUILDKITE_COMMIT" \
+  -F "run_env[number]=$BUILDKITE_BUILD_NUMBER" \
+  -F "run_env[job_id]=$BUILDKITE_JOB_ID" \
+  -F "run_env[message]=$BUILDKITE_MESSAGE" \
+  https://analytics-api.buildkite.com/v1/uploads
